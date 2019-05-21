853fd665ff6bf0a4eba44b77dfc42fa5f3cfb367
Kamal Singh Bora <48326644+thekamalbora@users.noreply.github.com>
2019-05-21 22:15:23 +0530

    Add files via upload

---
+++ upload-images.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="upload-images.aspx.cs" Inherits="BootstrapDynamicSlider.upload_images" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <title>Upload Images </title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="container" style="padding-top:10px">
+        <div class="form-horizontal">
+            <div class="form-group">
+                <asp:Label ID="Label1" runat="server" Text="Upload Image 1 " CssClass="col-md-2 control-label"></asp:Label>
+               <div class="col-md-3">
+                   <asp:FileUpload ID="img01" runat="server" CssClass="form-control"/>
+                   <asp:RequiredFieldValidator ID="RequiredFieldValidator1" 
+                       runat="server" ErrorMessage="This field is required" ControlToValidate="img01" CssClass="text-danger"></asp:RequiredFieldValidator>
+               </div>
+            </div>
+             <div class="form-group">
+                <asp:Label ID="Label2" runat="server" Text="Upload Image 2 " CssClass="col-md-2 control-label"></asp:Label>
+               <div class="col-md-3">
+                   <asp:FileUpload ID="img02" runat="server" CssClass="form-control"/>
+                   <asp:RequiredFieldValidator ID="RequiredFieldValidator2" 
+                       runat="server" ErrorMessage="This field is required" ControlToValidate="img02" CssClass="text-danger"></asp:RequiredFieldValidator>
+               </div>
+            </div>
+            <div class="form-group">
+                <asp:Label ID="Label3" runat="server" Text="Upload Image 3 " CssClass="col-md-2 control-label"></asp:Label>
+               <div class="col-md-3">
+                   <asp:FileUpload ID="img03" runat="server" CssClass="form-control"/>
+                   <asp:RequiredFieldValidator ID="RequiredFieldValidator3" 
+                       runat="server" ErrorMessage="This field is required" ControlToValidate="img03" CssClass="text-danger"></asp:RequiredFieldValidator>
+               </div>
+            </div>
+            <div class="form-group">
+                <asp:Label ID="Label4" runat="server" Text="Upload Image 4 " CssClass="col-md-2 control-label"></asp:Label>
+               <div class="col-md-3">
+                   <asp:FileUpload ID="img04" runat="server" CssClass="form-control"/>
+                   <asp:RequiredFieldValidator ID="RequiredFieldValidator4" 
+                       runat="server" ErrorMessage="This field is required" ControlToValidate="img04" CssClass="text-danger"></asp:RequiredFieldValidator>
+               </div>
+            </div>
+            <div class="form-group">
+                <asp:Label ID="Label5" runat="server" Text="Upload Image 5 " CssClass="col-md-2 control-label"></asp:Label>
+               <div class="col-md-3">
+                   <asp:FileUpload ID="img05" runat="server" CssClass="form-control"/>
+                   <asp:RequiredFieldValidator ID="RequiredFieldValidator5" 
+                       runat="server" ErrorMessage="This field is required" ControlToValidate="img05" CssClass="text-danger"></asp:RequiredFieldValidator>
+               </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-3 col-md-offset-2">
+                    <asp:Button ID="btnUpload" runat="server" Text="Uplaod Images" CssClass="btn btn-primary btn-group-lg" OnClick="btnUpload_Click"/>
+                    <asp:Label ID="lblMessage" runat="server" CssClass="text-success"></asp:Label>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ upload-images.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace BootstrapDynamicSlider
+{
+    public partial class upload_images : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                if (img01.HasFile)
+                {
+                    // Getting the image path.
+                    string SavePath = Server.MapPath("~/Images/");
+
+                    // Getting image name.
+                    string fileName = Path.GetFileNameWithoutExtension(img01.PostedFile.FileName);
+
+                    // Getting image extension
+                    string extension = Path.GetExtension(img01.PostedFile.FileName);
+
+                    //Saving images into folder.
+                    img01.SaveAs(SavePath + fileName + extension);
+
+                    SqlCommand cmd1 = new SqlCommand("INSERT INTO tblImages VALUES ('" + fileName + "','" + extension + "')", con);
+                    cmd1.ExecuteNonQuery();
+
+                }
+                if (img02.HasFile)
+                {
+                    // Getting the image path.
+                    string SavePath = Server.MapPath("~/Images/");
+
+                    // Getting image name.
+                    string fileName = Path.GetFileNameWithoutExtension(img02.PostedFile.FileName);
+
+                    // Getting image extension
+                    string extension = Path.GetExtension(img02.PostedFile.FileName);
+
+                    //Saving images into folder.
+                    img02.SaveAs(SavePath + fileName + extension);
+
+                    SqlCommand cmd2 = new SqlCommand("INSERT INTO tblImages VALUES ('" + fileName + "','" + extension + "')", con);
+                    cmd2.ExecuteNonQuery();
+
+                }
+                if (img03.HasFile)
+                {
+                    // Getting the image path.
+                    string SavePath = Server.MapPath("~/Images/");
+
+                    // Getting image name.
+                    string fileName = Path.GetFileNameWithoutExtension(img03.PostedFile.FileName);
+
+                    // Getting image extension
+                    string extension = Path.GetExtension(img02.PostedFile.FileName);
+
+                    //Saving images into folder.
+                    img03.SaveAs(SavePath + fileName + extension);
+
+                    SqlCommand cmd3 = new SqlCommand("INSERT INTO tblImages VALUES ('" + fileName + "','" + extension + "')", con);
+                    cmd3.ExecuteNonQuery();
+
+                }
+                if (img04.HasFile)
+                {
+                    // Getting the image path.
+                    string SavePath = Server.MapPath("~/Images/");
+
+                    // Getting image name.
+                    string fileName = Path.GetFileNameWithoutExtension(img04.PostedFile.FileName);
+
+                    // Getting image extension
+                    string extension = Path.GetExtension(img04.PostedFile.FileName);
+
+                    //Saving images into folder.
+                    img04.SaveAs(SavePath + fileName + extension);
+
+                    SqlCommand cmd4 = new SqlCommand("INSERT INTO tblImages VALUES ('" + fileName + "','" + extension + "')", con);
+                    cmd4.ExecuteNonQuery();
+
+                }
+                if (img05.HasFile)
+                {
+                    // Getting the image path.
+                    string SavePath = Server.MapPath("~/Images/");
+
+                    // Getting image name.
+                    string fileName = Path.GetFileNameWithoutExtension(img05.PostedFile.FileName);
+
+                    // Getting image extension
+                    string extension = Path.GetExtension(img05.PostedFile.FileName);
+
+                    //Saving images into folder.
+                    img05.SaveAs(SavePath + fileName + extension);
+
+                    SqlCommand cmd5 = new SqlCommand("INSERT INTO tblImages VALUES ('" + fileName + "','" + extension + "')", con);
+                    cmd5.ExecuteNonQuery();
+
+                }
+                lblMessage.Text = "Saved successfully.";
+            }
+        }
+    }
+}
